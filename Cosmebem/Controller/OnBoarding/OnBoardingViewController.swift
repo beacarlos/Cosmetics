0429489ffefda611935b591c33c719210dfb43f1
Beatriz Carlos <whobeatrizcarlos@gmail.com>
2020-08-24 13:07:04 -0300

    alterção de design

---
+++ Cosmebem/Controller/OnBoarding/OnBoardingViewController.swift
@@ -0,0 +1,30 @@
+//
+//  OnBoardingViewController.swift
+//  Cosmebem
+//
+//  Created by Beatriz Carlos on 24/08/20.
+//  Copyright © 2020 Beatriz Carlos. All rights reserved.
+//
+
+import UIKit
+
+class OnBoardingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Cosmebem/View/Onbording/OnBoardingView.swift
@@ -0,0 +1,21 @@
+//
+//  OnBoardingView.swift
+//  Cosmebem
+//
+//  Created by Beatriz Carlos on 24/08/20.
+//  Copyright © 2020 Beatriz Carlos. All rights reserved.
+//
+
+import UIKit
+
+class OnBoardingView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
